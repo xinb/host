e3f9b1b6685c5ffe87d4f09f2a3370a12876e8c2
xinb <24700858+xinb@users.noreply.github.com>
2021-03-18 14:30:37 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-./server/xray -config ./server/server.json > p server.file 2>&1 &
-./client/xray -config ./client/client.json > p client.file 2>&1 &
-iperf3 -siperf3 -s > p iperf3_server.file 2>&1 &
-proxychains  iperf3 -c 127.0.0.1 -t 10 > p iperf3_client.file 2>&1 &
+./server/xray -config ./server/server.json > server.file 2>&1 &
+./client/xray -config ./client/client.json > client.file 2>&1 &
+iperf3 -siperf3 -s > iperf3_server.file 2>&1 &
+proxychains  iperf3 -c 127.0.0.1 -t 10 > iperf3_client.txt 2>&1 &
