36f6a5b8bc2164cd6bd8cd58baed26a849c58757
xinb <24700858+xinb@users.noreply.github.com>
2021-03-18 14:43:56 +0800

    update

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
+apt install iperf3
+apt install proxychains 
+apt install cpulimit
+wget https://raw.githubusercontent.com/xinb/host/main/proxychains.conf
+cp ./proxychains.conf /etc/proxychains.conf
 ./server/xray -config ./server/server.json > server.file 2>&1 &
 ./client/xray -config ./client/client.json > client.file 2>&1 &
-iperf3 -siperf3 -s > iperf3_server.file 2>&1 &
-proxychains  iperf3 -c 127.0.0.1 -t 10 > iperf3_client.txt 2>&1 &
+iperf3 -siperf3 -s > test_iperf3_server.file 2>&1 &
+proxychains  iperf3 -c 127.0.0.1 -t 10 > test_iperf3_client.txt 2>&1 &
